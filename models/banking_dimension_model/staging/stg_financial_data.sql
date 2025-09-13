cd653e09b204a68c9472ef37eb74d1fad7903861
Sandeep Nair <sndataspecialist@gmail.com>
2025-09-13 20:03:40 +0000

    ASDASD

--- models/banking_dimension_model/staging/stg_financial_data.sql
+++ models/banking_dimension_model/staging/stg_financial_data.sql
@@ -3,3 +3,4 @@
 -- models/staging/stg_customers.sql
 select *
 from {{ source('bronze', 'FinancialData') }}
+WHERE TransactionID not in (select TransactionID from BANKING_DWH.GOLD.FACT_BANK_ASSET_DETAILS)
--- models/banking_dimension_model/staging/stg_transactions.sql
+++ models/banking_dimension_model/staging/stg_transactions.sql
@@ -3,3 +3,4 @@
 -- models/staging/stg_customers.sql
 select *
 from {{ source('bronze', 'Transactions') }}
+WHERE TransactionID not in (select TransactionID from BANKING_DWH.GOLD.FACT_TRANSCATIONS)
